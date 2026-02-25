bfb5a5d0b77a3caf2bb33c5c51b738642f2d7e74
akdas <ahmadakdas07@gmail.com>
2026-02-26 01:32:35 +0530

    working in intellij ide

--- price-processor-service/src/main/java/com/akdas/stock/priceprocessor/config/RedisConfig.java
+++ price-processor-service/src/main/java/com/akdas/stock/priceprocessor/config/RedisConfig.java
@@ -1,19 +1,36 @@
 package com.akdas.stock.priceprocessor.config;
 
+import com.akdas.stock.priceprocessor.model.PriceTick;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(
+    public RedisTemplate<String, PriceTick> redisTemplate(
             RedisConnectionFactory factory) {
 
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        Jackson2JsonRedisSerializer<PriceTick> valueSerializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, PriceTick.class);
+
+        RedisTemplate<String, PriceTick> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(valueSerializer);
+        template.setHashValueSerializer(valueSerializer);
         return template;
     }
 }
